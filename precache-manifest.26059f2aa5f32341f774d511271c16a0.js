ea89c357632d159118192088e5682f48bbb434c6
Martin Di iorio <martin@coderteams.com>
2019-02-04 03:40:35 -0300

    Updates

---
+++ precache-manifest.26059f2aa5f32341f774d511271c16a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a32f8fe521c9a95db9d",
+    "url": "/swapi/static/js/runtime~main.5a32f8fe.js"
+  },
+  {
+    "revision": "f8e73b116200b7bc5da5",
+    "url": "/swapi/static/js/main.f8e73b11.chunk.js"
+  },
+  {
+    "revision": "6b4ac77795d1b11c5d21",
+    "url": "/swapi/static/js/1.6b4ac777.chunk.js"
+  },
+  {
+    "revision": "f8e73b116200b7bc5da5",
+    "url": "/swapi/static/css/main.77a9c5f9.chunk.css"
+  },
+  {
+    "revision": "6b4ac77795d1b11c5d21",
+    "url": "/swapi/static/css/1.e06ddba9.chunk.css"
+  },
+  {
+    "revision": "fab1afa7642779a6bfddc8c424eb92b7",
+    "url": "/swapi/index.html"
+  }
+];
